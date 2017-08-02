9f81bb2cdc7396a15df6a8b5185a3b2c0e25b6a6
Roman Kopaev <roman@kopaev.ru>
2017-08-02 11:34:15 +0300

    no message

---
+++ Source/PZone.Core/Exceptions/CodeUsageException.cs
@@ -0,0 +1,19 @@
+﻿namespace PZone.Exceptions
+{
+    /// <summary>
+    /// Ошибка использования компонентов кода.
+    /// </summary>
+    /// <remarks>
+    /// Ошибочное использование классов и программных конструкций.
+    /// </remarks>
+    public class CodeUsageException : CommonException
+    {
+        /// <summary>
+        /// Конструтор класса.
+        /// </summary>
+        /// <param name="message">Сообщение об ошибке.</param>
+        public CodeUsageException(string message) : base(10000, message)
+        {
+        }
+    }
+}
