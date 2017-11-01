8fcbf130dffc9955f4b8a476b5eef89b884299dc
Roman Kopaev <roman@kopaev.ru>
2017-11-01 13:59:49 +0300

    ADD Добавлено перечисление с единицами измерения времени.

---
+++ Source/PZone.Core/TimeUnit.cs
@@ -0,0 +1,58 @@
+﻿namespace PZone
+{
+    /// <summary>
+    /// Единицы измерения времени.
+    /// </summary>
+    public enum TimeUnit
+    {
+        /// <summary>
+        /// Миллисекунда.
+        /// </summary>
+        Millisecond,
+
+        /// <summary>
+        /// Секунда.
+        /// </summary>
+        Second,
+
+        /// <summary>
+        /// Минута.
+        /// </summary>
+        Minute,
+
+        /// <summary>
+        /// Час.
+        /// </summary>
+        Hour,
+
+        /// <summary>
+        /// День.
+        /// </summary>
+        Day,
+
+        /// <summary>
+        /// Неделя.
+        /// </summary>
+        Week,
+
+        /// <summary>
+        /// Месяц.
+        /// </summary>
+        Month,
+
+        /// <summary>
+        /// Квартал.
+        /// </summary>
+        Quarter,
+
+        /// <summary>
+        /// Год.
+        /// </summary>
+        Year,
+
+        /// <summary>
+        /// Век.
+        /// </summary>
+        Century
+    }
+}
